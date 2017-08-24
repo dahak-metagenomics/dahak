053f3a7c564191043d8d7c9e1ef8b1b5f017763c
Phillip Brooks <philliptbrooks@gmail.com>
2017-08-24 12:17:26 -0700

    Create assemble.sh
    Assembles filtered paired-end reads

---
+++ scripts/assemble.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+i="SRR606249"
+
+#### from ubuntu 16.04 using filtered reads
+docker pull quay.io/biocontainers/megahit:1.1.1--py36_0
+docker pull quay.io/biocontainers/spades:3.10.1--py27_0
+docker pull quay.io/biocontainers/quast:4.5--boost1.61_1
+
+#### link the data and run Megahit
+docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/megahit:1.1.1--py36_0 megahit --12 /data/$i.pe.trim.fq.gz -o /data/megahit_output_podar_metaG_100
+
+#### Now run quast to generate some assembly statistics 
+docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 quast.py /data/megahit_output_podar_metaG_100/final.contigs.fa -o /data/megahit_output_podar_metaG_100_quast_report
+
+#### link the data and run SPAdes 
+docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/spades:3.10.1--py27_0 metaspades.py --12 /data/$i.pe.trim.fq.gz \
+	-o /data/spades_output_podar_metaG
+
+#### Now run quast to generate some assembly statistics 
+docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 quast.py /data/spades_ouput_podar_metaG_sub_10/contigs.fasta -o data/spades_output_podar_metaG_100_quast_report
