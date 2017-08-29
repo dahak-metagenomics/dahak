16d4a02fab8b3b48c0ef189e46b79ece92755b8f
Phillip Brooks <philliptbrooks@gmail.com>
2017-08-29 10:47:36 -0700

    Update assemble.sh

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i="SRR606249"
+i="SRR606249_subset50"
 
 #### from ubuntu 16.04 using filtered reads
 docker pull quay.io/biocontainers/megahit:1.1.1--py36_0
@@ -9,16 +9,24 @@ docker pull quay.io/biocontainers/quast:4.5--boost1.61_1
 
 #### link the data and run Megahit
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/megahit:1.1.1--py36_0 \
-       'megahit --12 /data/$i.pe.trim.fq.gz -o /data/megahit_output_podar_metaG_100'
+    megahit --12 /data/${i}.pe.trim.fq.gz -o /data/megahit_output_podar_metaG_50
 
 #### Now run quast to generate some assembly statistics 
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 \
-       'quast.py /data/megahit_output_podar_metaG_100/final.contigs.fa -o /data/megahit_output_podar_metaG_100_quast_report'
+    quast.py /data/megahit_output_podar_metaG_50/final.contigs.fa -o /data/megahit_output_podar_metaG_50_quast_report
 
 #### link the data and run SPAdes 
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/spades:3.10.1--py27_0 \
-       'metaspades.py --12 /data/$i.pe.trim.fq.gz -o /data/spades_output_podar_metaG'
+    metaspades.py --12 /data/${i}.pe.trim.fq.gz -o /data/spades_output_podar_metaG_50
 
 #### Now run quast to generate some assembly statistics 
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 \
-       'quast.py /data/spades_ouput_podar_metaG_sub_10/contigs.fasta -o data/spades_output_podar_metaG_100_quast_report'
+    quast.py /data/spades_output_podar_metaG_50/contigs.fasta -o data/spades_output_podar_metaG_50_quast_report
+
+docker pull thanhleviet/abricate abricate
+docker pull ummidock/prokka:1.12
+
+docker run -v /home/ubuntu/data:/data -it ummidock/prokka:1.12 \
+    prokka /data/megahit_output_podar_metaG_50/final.contigs.fa --outdir /data/prokka_annotation --prefix podar_metaG_50
+docker run -v /home/ubuntu/data:/data -it ummidock/prokka:1.12 \
+    prokka /data/spades_output_podar_metaG_50/contigs.fasta --outdir /data/prokka_annotation --prefix podar_metaG_sub_50
