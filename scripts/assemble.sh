5e8aeaa3eb6ce047af3cce3a0bc031daae0cf821
Ubuntu <ubuntu@ip-172-30-0-247.ec2.internal>
2017-09-13 20:32:02 +0000

    added file containing all file names for download using forloop in protocol

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -12,21 +12,21 @@ docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/megahit:1.1.1--p
     megahit --12 /data/${i}.pe.trim.fq.gz -o /data/megahit_output_podar_metaG_100
 
 #### Now run quast to generate some assembly statistics 
-docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 \
-    quast.py /data/megahit_output_podar_metaG_100/final.contigs.fa -o /data/megahit_output_podar_metaG_100_quast_report
+#docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 \
+#    quast.py /data/megahit_output_podar_metaG_100/final.contigs.fa -o /data/megahit_output_podar_metaG_100_quast_report
 
 #### link the data and run SPAdes 
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/spades:3.10.1--py27_0 \
     metaspades.py --12 /data/${i}.pe.trim.fq.gz -o /data/spades_output_podar_metaG_100
 
 #### Now run quast to generate some assembly statistics 
-docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 \
-    quast.py /data/spades_output_podar_metaG_100/contigs.fasta -o data/spades_output_podar_metaG_100_quast_report
+#docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/quast:4.5--boost1.61_1 \
+#    quast.py /data/spades_output_podar_metaG_100/contigs.fasta -o data/spades_output_podar_metaG_100_quast_report
 
-docker pull thanhleviet/abricate abricate
-docker pull ummidock/prokka:1.12
+#docker pull thanhleviet/abricate abricate
+#docker pull ummidock/prokka:1.12
 
-docker run -v /home/ubuntu/data:/data -it ummidock/prokka:1.12 \
-    prokka /data/megahit_output_podar_metaG_100/final.contigs.fa --outdir /data/prokka_megahit_100_annotation --prefix podar_metaG_100
-docker run -v /home/ubuntu/data:/data -it ummidock/prokka:1.12 \
-    prokka /data/spades_output_podar_metaG_100/contigs.fasta --outdir /data/prokka_spades_100_annotation --prefix podar_metaG_sub_100
+#docker run -v /home/ubuntu/data:/data -it ummidock/prokka:1.12 \
+#    prokka /data/megahit_output_podar_metaG_100/final.contigs.fa --outdir /data/prokka_megahit_100_annotation --prefix podar_metaG_100
+#docker run -v /home/ubuntu/data:/data -it ummidock/prokka:1.12 \
+#    prokka /data/spades_output_podar_metaG_100/contigs.fasta --outdir /data/prokka_spades_100_annotation --prefix podar_metaG_sub_100
--- scripts/filtering.sh
+++ scripts/filtering.sh
@@ -13,24 +13,24 @@ docker pull quay.io/biocontainers/pandaseq:2.11--1
 #
 mkdir data
 cd data
-#osf -p dm938 fetch osfstorage/${i}_1.fq.gz
-#osf -p dm938 fetch osfstorage/${i}_2.fq.gz
+osf -p dm938 fetch osfstorage/data/${i}_1.fq.gz
+osf -p dm938 fetch osfstorage/data/${i}_2.fq.gz
 
 #### Link the data and run fastqc
-mkdir qc
-mkdir qc/before_trim 
-cd qc/before_trim 
+#mkdir qc
+#mkdir qc/before_trim 
+#cd qc/before_trim 
 
 
-docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
-    fastqc /data/${i}_1.fq.gz -o /data/qc/before_trim
+#docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
+#    fastqc /data/${i}_1.fq.gz -o /data/qc/before_trim
       
-docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
-    fastqc /data/${i}_2.fq.gz -o /data/qc/before_trim
+#docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
+#    fastqc /data/${i}_2.fq.gz -o /data/qc/before_trim
 
 #### Download the adapters 
-cd ~/data
-curl -O -L http://dib-training.ucdavis.edu.s3.amazonaws.com/mRNAseq-semi-2015-03-04/TruSeq2-PE.fa
+#cd ~/data
+#curl -O -L http://dib-training.ucdavis.edu.s3.amazonaws.com/mRNAseq-semi-2015-03-04/TruSeq2-PE.fa
 
 #### Run trimmomatic
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/trimmomatic:0.36--4 \
@@ -41,18 +41,18 @@ docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/trimmomatic:0.36
         ILLUMINACLIP:/data/TruSeq2-PE.fa:2:40:15 \
         LEADING:2 TRAILING:2 \
         SLIDINGWINDOW:4:2 \
-        MINLEN:25
+#        MINLEN:25
 
 #### Create a directory for the trimmed data and run fastqc again         
-mkdir ~/data/qc/after_trim
-cd qc/after_trim
+#mkdir ~/data/qc/after_trim
+#cd qc/after_trim
 
-docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
-    fastqc /data/${i}_1.trim.fq.gz -o /data/qc/after_trim
-docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
-    fastqc /data/${i}_2.trim.fq.gz -o /data/qc/after_trim
+#docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
+#    fastqc /data/${i}_1.trim.fq.gz -o /data/qc/after_trim
+#docker run -v /home/ubuntu/data:/data -it biocontainers/fastqc \
+#    fastqc /data/${i}_2.trim.fq.gz -o /data/qc/after_trim
 
 #### Interleave the reads                                                                                   
-cd ~/data
+#cd ~/data
 docker run -v /home/ubuntu/data:/data -it quay.io/biocontainers/khmer:2.1--py35_0 \
     interleave-reads.py /data/${i}_1.trim.fq.gz /data/${i}_2.trim.fq.gz --no-reformat -o /data/${i}.pe.trim.fq.gz --gzip
