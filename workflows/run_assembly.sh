47da82c8d1f98e902acb6accc9caaff30452abc1
Charles Reid <charlesreid1@gmail.com>
2018-06-18 11:03:49 -0700

    migrate run scripts to top-level workflow directory

---
+++ workflows/run_assembly.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#snakemake --forceall --dag sbts | dot -Tpdf > dag.pdf
+
+#SINGULARITY_BINDPATH="data:/data" snakemake -n --use-singularity --printshellcmds metaspades
+SINGULARITY_BINDPATH="data:/data" snakemake --use-singularity --printshellcmds metaspades
+
