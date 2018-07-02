1fd258d006c643ed464ed1ae7e03317b86f9bd7f
Charles Reid <charlesreid1@gmail.com>
2018-07-02 10:49:36 -0700

    fix conflicting assembly/read filtering wildcards

--- workflows/test_assembly.sh
+++ workflows/test_assembly.sh
@@ -6,4 +6,6 @@
 #SINGULARITY_BINDPATH="data:/data" snakemake --use-singularity --printshellcmds metaspades
 
 #snakemake -n -p metaspades
-snakemake -n -p megahit
+#snakemake -n -p megahit
+snakemake -n -p data/SRR606249_subset10_trim30_metaspades.contigs.fa
+#snakemake -l
--- workflows/test_readfilt.sh
+++ workflows/test_readfilt.sh
@@ -5,5 +5,7 @@
 #SINGULARITY_BINDPATH="data:/data" snakemake -n -p --use-singularity pre_trim
 #SINGULARITY_BINDPATH="data:/data" snakemake -n -p --use-singularity post_trim
 
-snakemake -n -p pre_trim
-snakemake -n -p post_trim
+#snakemake -n -p pre_trim
+#snakemake -n -p post_trim
+
+snakemake -n -p data/SRR606249_subset10_2_trim30.fq.gz
