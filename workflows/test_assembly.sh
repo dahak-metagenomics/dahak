5e2bb4151224c37725ae69411bdda581e1ac8ca9
Charles Reid <charlesreid1@gmail.com>
2018-07-03 09:36:19 -0700

    update test scripts

--- workflows/test_assembly.sh
+++ workflows/test_assembly.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#snakemake --forceall --dag sbts | dot -Tpdf > dag.pdf
+#SINGULARITY_BINDPATH="data:/data" snakemake -n -p --use-singularity metaspades
+#SINGULARITY_BINDPATH="data:/data" snakemake -p --use-singularity metaspades
 
-#SINGULARITY_BINDPATH="data:/data" snakemake -n --use-singularity --printshellcmds metaspades
-#SINGULARITY_BINDPATH="data:/data" snakemake --use-singularity --printshellcmds metaspades
+snakemake -n -p assembly_workflow_metaspades
+snakemake -n -p assembly_workflow_megahit
 
-#snakemake -n -p metaspades
-#snakemake -n -p megahit
-snakemake -n -p data/SRR606249_subset10_trim30_metaspades.contigs.fa
-#snakemake -l
