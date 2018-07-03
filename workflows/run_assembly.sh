009a0abf2fcaaddf7c6bd887891f5a211b45b380
Charles Reid <charlesreid1@gmail.com>
2018-07-03 10:18:29 -0700

    add run assembly script

---
+++ workflows/run_assembly.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SINGULARITY_BINDPATH="data:/data" snakemake -p --use-singularity assembly_workflow_metaspades 
+SINGULARITY_BINDPATH="data:/data" snakemake -p --use-singularity assembly_workflow_megahit
+
