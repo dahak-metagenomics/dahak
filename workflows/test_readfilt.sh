17a259602e0f8e5957ae4460daf1f185a62f8b3c
Charles Reid <charlesreid1@gmail.com>
2018-07-06 14:10:33 -0700

    nicer bash style

--- workflows/test_readfilt.sh
+++ workflows/test_readfilt.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-#snakemake -n -p read_filtering_pretrim_workflow
-#snakemake --forceall --dag read_filtering_pretrim_workflow | dot -Tpdf > dag_readfilt_pre.pdf
-#echo "----------------------"
-#echo "rule: read_filtering_workflow"
-#echo "task graph: dag_readfilt_pre.pdf"
-#echo "----------------------"
-#echo ""
+function main() {
+    pretrim
+    posttrim
+}
 
-snakemake -n -p read_filtering_posttrim_workflow
-snakemake --forceall --dag read_filtering_posttrim_workflow | dot -Tpdf > dag_readfilt_post.pdf
-echo "----------------------"
-echo "rule: read_filtering_posttrim_workflow"
-echo "task graph: dag_readfilt_post.pdf"
-echo "----------------------"
-echo ""
+function pretrim() {
+    snakemake -n -p read_filtering_pretrim_workflow
+    snakemake --forceall --dag read_filtering_pretrim_workflow | dot -Tpdf > dag_readfilt_pre.pdf
+    echo "----------------------"
+    echo "rule: read_filtering_workflow"
+    echo "task graph: dag_readfilt_pre.pdf"
+    echo "----------------------"
+    echo ""
+}
+
+function posttrim() {
+    snakemake -n -p read_filtering_posttrim_workflow
+    snakemake --forceall --dag read_filtering_posttrim_workflow | dot -Tpdf > dag_readfilt_post.pdf
+    echo "----------------------"
+    echo "rule: read_filtering_posttrim_workflow"
+    echo "task graph: dag_readfilt_post.pdf"
+    echo "----------------------"
+    echo ""
+}
+
+main
