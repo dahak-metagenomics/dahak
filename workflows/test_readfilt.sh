5553a8b2d3859d3fb73b6ab5de1db1613abc86d8
Charles Reid <charlesreid1@gmail.com>
2018-07-06 14:02:46 -0700

    fix issue with wildcard-matching in read filtering workflows

--- workflows/test_readfilt.sh
+++ workflows/test_readfilt.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-snakemake -n -p read_filtering_workflow
-snakemake --forceall --dag read_filtering_workflow | dot -Tpdf > dag_readfilt.pdf
+#snakemake -n -p read_filtering_pretrim_workflow
+#snakemake --forceall --dag read_filtering_pretrim_workflow | dot -Tpdf > dag_readfilt_pre.pdf
+#echo "----------------------"
+#echo "rule: read_filtering_workflow"
+#echo "task graph: dag_readfilt_pre.pdf"
+#echo "----------------------"
+#echo ""
+
+snakemake -n -p read_filtering_posttrim_workflow
+snakemake --forceall --dag read_filtering_posttrim_workflow | dot -Tpdf > dag_readfilt_post.pdf
 echo "----------------------"
-echo "rule: read_filtering_workflow"
-echo "task graph: dag_readfilt.pdf"
+echo "rule: read_filtering_posttrim_workflow"
+echo "task graph: dag_readfilt_post.pdf"
 echo "----------------------"
 echo ""
-
