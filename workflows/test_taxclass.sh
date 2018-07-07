4bc0b3239081d7cf690d05dd5db85c65a7ccd519
Charles Reid <charlesreid1@gmail.com>
2018-07-07 10:24:47 -0700

    Fixing wildcards broken by last wildcard fix.

--- workflows/test_taxclass.sh
+++ workflows/test_taxclass.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
-snakemake -n -p taxonomic_classification_workflow
-snakemake --forceall --dag taxonomic_classification_workflow | dot -Tpdf > dag_taxcass.pdf
-echo "----------------------"
-echo "rule: taxonomic_classification_workflow"
-echo "task graph: dag_taxclass.pdf"
-echo "----------------------"
-echo ""
+function main() {
+    #taxclass_signatures
+    #taxclass_filter
+    taxclass_gather
+    #taxclass
+}
 
-#snakemake -n -p runkaiju
-#snakemake -n -p runkrona
+function taxclass_signatures() {
+    snakemake -n -p taxonomic_classification_workflow_signatures
+    snakemake --forceall --dag taxonomic_classification_workflow_signatures | dot -Tpdf > dag_taxclass_sig.pdf
+    echo "----------------------"
+    echo "rule: taxonomic_classification_sig"
+    echo "task graph: dag_taxclass_sig.pdf"
+    echo "----------------------"
+    echo ""
+}
 
+function taxclass_gather() {
+    snakemake -n -p taxonomic_classification_gather_workflow
+    snakemake --forceall --dag taxonomic_classification_gather_workflow | dot -Tpdf > dag_taxclass_gather.pdf
+    echo "----------------------"
+    echo "rule: taxonomic_classification_gather_workflow"
+    echo "task graph: dag_taxclass_gather.pdf"
+    echo "----------------------"
+    echo ""
+}
+
+function taxclass_filter() {
+    snakemake -n -p taxonomic_classification_filter_workflow
+    snakemake --forceall --dag taxonomic_classification_filter_workflow | dot -Tpdf > dag_taxclass_filter.pdf
+    echo "----------------------"
+    echo "rule: taxonomic_classification_filter_workflow"
+    echo "task graph: dag_taxclass_filter.pdf"
+    echo "----------------------"
+    echo ""
+}
+
+function taxclass() {
+    snakemake -n -p taxonomic_classification_workflow
+    snakemake --forceall --dag taxonomic_classification_workflow | dot -Tpdf > dag_taxclass.pdf
+    echo "----------------------"
+    echo "rule: taxonomic_classification_workflow"
+    echo "task graph: dag_taxclass.pdf"
+    echo "----------------------"
+    echo ""
+}
+
+main
