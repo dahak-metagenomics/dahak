f59d52984672478be453dac0f71b502d7df1f0bd
Charles Reid <charlesreid1@gmail.com>
2018-07-24 20:04:08 -0400

    slurm read filtering should use long forms of -n and -p

--- workflows/slurm_readfiltering.sh
+++ workflows/slurm_readfiltering.sh
@@ -5,7 +5,7 @@
 
 function main() {
     pretrim
-    #posttrim
+    posttrim
 }
 
 function pretrim() {
@@ -24,26 +24,13 @@ function run() {
     CLUSTER_JSON="slurm/cluster.json"
     JS="slurm/jobscript.sh"
 
-    snakemake -p \
+    snakemake --dryrun --printshellcmds \
         -j 100 \
         --cluster-config ${CLUSTER_JSON} \
         --js ${JS} \
         --cluster "sbatch -A {cluster.account} -n {cluster.n} --export=SINGULARITY_BINDPATH={cluster.singularity_bindpath} --partition {cluster.partition} --mem {cluster.mem} -t {cluster.time}" \
         --use-singularity \
         ${target} 
-
-    ### https://github.com/dib-lab/2017-paper-gather/blob/master/pipeline/hpcc/submit.sh
-    ###
-    ###snakemake                               \
-    ###    -j 100                              \
-    ###    --cluster-config hpcc/cluster.yaml  \
-    ###    --js hpcc/jobscript.sh              \
-    ###    --rerun-incomplete                  \
-    ###    --keep-going                        \
-    ###    --latency-wait 10                   \
-    ###    --max-jobs-per-second 1             \
-    ###    --use-conda                         \
-    ###    --cluster "$QSUB" $@
 }
 
 main
