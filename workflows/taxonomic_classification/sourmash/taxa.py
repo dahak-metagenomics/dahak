ff2ee9668c09e8398ea73e2ee7b23983a97f81b8
Phillip Brooks <ptbrooks@ucdavis.edu>
2018-08-03 14:36:00 -0700

    added code to calculate presence absence table

--- workflows/taxonomic_classification/sourmash/taxa.py
+++ workflows/taxonomic_classification/sourmash/taxa.py
@@ -2,3 +2,16 @@ import seaborn as sns
 import pandas as pd
 import os
 import glob
+
+def concat_sourmash_csv(filenames):
+    x = glob.glob(filenames)
+    list_of_dfs = [pd.read_csv(filename, header = 0) for filename in x]
+    for dataframe, filename in zip(list_of_dfs, x):
+        dataframe['filename'] = filename
+    combined_df = pd.concat(list_of_dfs, ignore_index=True)
+    return combined_df
+
+def plot_otu_presence_absence(filenames):
+    combined_df = concat_sourmash_csv(filenames)
+    plot = combined_df['name'].value_counts().plot(kind="bar", figsize = (60,8))
+    return plot 
